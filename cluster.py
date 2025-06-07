e75e188e94a6475e9e79bc0d853bd3c1b3c60d98
pogorzelskich <106491946+pogorzelskich@users.noreply.github.com>
2025-06-07 23:24:35 +0300

    Новый алгоритм локтя

--- cluster.py
+++ cluster.py
@@ -1,43 +1,52 @@
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+from kneed import KneeLocator
 import numpy as np
 from typing import List, Tuple
+from tqdm import tqdm
 import warnings
 warnings.filterwarnings('ignore')
 
 
-def determine_optimal_clusters_elbow(latents: np.ndarray, max_k: int = 50) -> int:
+def determine_optimal_clusters_elbow(latents: np.ndarray, max_k: int = 30) -> int:
     """Определить оптимальное количество кластеров, используя метод локтя."""
-    n_samples = latents.shape[0]
-    max_k = min(max_k, n_samples // 2)
+    n_samples = len(latents)
+    if n_samples < 3:
+        return 2
     
+    # k_range = range(2, max_k + 1)
+    max_k = max(4, min(len(latents) - 1, (len(latents) // 10) + 2, max_k))
+    k_range = range(2, min(max_k + 1, len(latents), 100))
     inertias = []
-    k_range = range(1, max_k + 1)
     
-    for k in k_range:
+    for k in tqdm(k_range):
         kmeans = KMeans(n_clusters=k, random_state=42, n_init=10)
         kmeans.fit(latents)
         inertias.append(kmeans.inertia_)
-    
-    # Найдти "локоть" с помощью второй производной
-    deltas = np.diff(inertias)
-    delta2 = np.diff(deltas)
-    optimal_k = np.argmin(delta2) + 2  # +2 из-за второй производной (каждая удаляет по одному элементу)
-    
-    return max(2, optimal_k)
 
-def cluster_latent_vectors(images_and_latents: List[Tuple[str, np.ndarray]]) -> \
+    knee = KneeLocator(list(k_range), inertias, curve="convex", direction="decreasing")
+    optimal_k = knee.elbow
+
+    if optimal_k is None:
+        optimal_k = 2
+    return optimal_k
+
+
+def cluster_latent_vectors(images_and_latents: List[Tuple[str, np.ndarray]], print_process=False) -> \
         Tuple[str, Tuple[Tuple[str, float, bool], ...]]:
     """
     Кластеризовать латентные векторы с использованием метода адаптивного расстояния с запеченными параметрами.
     
     Входные данные:
         images_and_latents: Список из кортежей (имя файла, латентный вектор)
+        print_process: bool (по умолчанию False) -- печатает в консоль процесс выполнения
     
     Выходные данные:
         Кластеры: (имя кластера, ((имя файла, вероятность, входит ли в кластер), ...))
     """
     
+    print("Начало кластеризации") if print_process else None
+
     # Разделение латентов и имён файлов
     filenames = [item[0] for item in images_and_latents]
     latents = np.array([item[1] for item in images_and_latents])
@@ -47,18 +56,21 @@ def cluster_latent_vectors(images_and_latents: List[Tuple[str, np.ndarray]]) -> 
     latents_scaled = scaler.fit_transform(latents)
     
     # Расчёт оптимального количества k с помощью метода локтя
-    max_k = min(len(filenames), (len(filenames) // 3) + 2)
-    optimal_k = determine_optimal_clusters_elbow(latents_scaled, max_k=max_k)
+    max_k = min(len(filenames), (len(filenames) // 20) + 2, 100)
+    optimal_k = determine_optimal_clusters_elbow(latents_scaled, max_k=len(filenames))
+    print("optimal_k (Расчёт оптимального количества k с помощью метода локтя) завершено") if print_process else None
     
     # K-means центры кластеров
     kmeans = KMeans(n_clusters=optimal_k, random_state=42, n_init=10)
     kmeans.fit(latents_scaled)
     centers = kmeans.cluster_centers_
+    print("kmeans (K-means центры кластеров) завершено") if print_process else None
     
     # Расчёт расстояний от центров кластеров каждой точки
     distances = np.zeros((len(latents_scaled), optimal_k))
     for i, center in enumerate(centers):
         distances[:, i] = np.linalg.norm(latents_scaled - center, axis=1)
+    print("distances (Расчёт расстояний от центров кластеров каждой точки) завершено") if print_process else None
     
     # Расчёт адаптивного порог для каждого кластера (75-ый процентиль)
     cluster_thresholds = []
@@ -66,6 +78,7 @@ def cluster_latent_vectors(images_and_latents: List[Tuple[str, np.ndarray]]) -> 
         cluster_distances = distances[:, cluster_id]
         threshold = np.percentile(cluster_distances, 75)  # distance_percentile=75
         cluster_thresholds.append(threshold)
+    print("cluster_thresholds (Расчёт адаптивного порог для каждого кластера) завершено") if print_process else None
     
     # Преобразование расстояний в вероятности с помощью адаптивных порогов
     probabilities = np.zeros_like(distances)
@@ -75,9 +88,10 @@ def cluster_latent_vectors(images_and_latents: List[Tuple[str, np.ndarray]]) -> 
         cluster_distances = distances[:, cluster_id]
         threshold = cluster_thresholds[cluster_id]
         
-        # Вероятность с экспоненциальым затуханием
+        # Вероятность с экспоненциальным затуханием
         normalized_distances = (cluster_distances / threshold) ** 2
         probabilities[:, cluster_id] = np.exp(-normalized_distances / temperature)
+    print("probabilities (Преобразование расстояний в вероятности с помощью адаптивных порогов) завершено") if print_process else None
     
     # Организация кластеров (список)
     clusters = []
@@ -98,5 +112,7 @@ def cluster_latent_vectors(images_and_latents: List[Tuple[str, np.ndarray]]) -> 
         # Сопртировка по самым вероятным значениям
         cluster_members.sort(key=lambda x: -x[1])
         clusters.append(("", tuple(cluster_members)))  # Имя (пустое) и расчёты
+
+        print(f"cluster_members (Организация кластера {cluster_id+1}/{optimal_k}) завершено") if print_process else None
     
     return clusters
