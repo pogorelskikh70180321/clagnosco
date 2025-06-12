6eb9d5ac084cb4dc532f19910393e880d1817adb
pogorzelskich <106491946+pogorzelskich@users.noreply.github.com>
2025-06-12 04:33:07 +0300

    Глобальные переменные перенесены в отдельный .js файл

---
+++ webui/static/vars.js
@@ -0,0 +1,13 @@
+var safeReload = true;
+var loggingFetch = true;
+var serverConnect = true;
+var serverReconnect = false;
+var reloadNeeded = false;
+
+var isSaving = false;
+
+var clagnoscoClassesSizes = [];
+var clagnoscoImagesNames = [];
+var imagesFolder;
+const imgView = '/img/';
+const smallImgView = '/img_small/';
